42ae6e4dd87ed26c1f92248a16a1bbee4f3c4050
kptudyogamela <kptudyogamela@gmail.com>
2025-06-14 11:27:24 +0530

    updated url

--- server/server.js
+++ server/server.js
@@ -17,7 +17,9 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173, 'https://healthmix-frontend.vercel.app",
+];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 app.use(express.json());
